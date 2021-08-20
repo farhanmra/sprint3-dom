af77409146c8c76ba6f7c8255122902a24bade07
Farhan M R A <48043106+farhanmra@users.noreply.github.com>
2021-08-20 21:07:54 +0700

    update 1

--- dom-events/events.js
+++ dom-events/events.js
@@ -1,3 +1,50 @@
 //events handler
+const p3=document.getElementsByClassName('p3')[0]
+
+function ubahWarna(){
+    p3.style.backgroundColor='orange'
+}
+
+p3.onclick=ubahWarna
+
+
+const alif=document.querySelector('#b ul li:nth-child(2)')
+function ubahWarna2(){
+    alif.style.backgroundColor='green'
+}
+
+alif.onclick=ubahWarna2
 
 //addEventListener()
+
+const p4=document.querySelector('#b')
+
+p4.addEventListener('click',function(){
+    
+    const newLi=document.createElement('li')
+    const isiLi=document.createTextNode('List baru nih')
+    const listBaru=document.querySelector('#b ul')
+    newLi.appendChild(isiLi)
+    listBaru.appendChild(newLi)
+    p4.appendChild(listBaru)
+
+})
+
+
+//latihan 
+sectionC=document.createElement('section')
+sectionC.setAttribute('id','c')
+const container=document.getElementById('container')
+container.appendChild(sectionC)
+
+const button=document.createElement('button')
+const isianButton=document.createTextNode('Tombol hijau')
+button.appendChild(isianButton)
+sectionC.appendChild(button)
+
+const btn=document.querySelector('#c button')
+
+btn.addEventListener('click',function(){
+    ubah=document.getElementById('container')
+    ubah.style.backgroundColor='green'
+})
--- dom-manipulation/manipulation.js
+++ dom-manipulation/manipulation.js
@@ -35,7 +35,65 @@ jojo.classList.replace('jojo','akufarhan')
 // Node Manipulation
 //document.createElement()
 //document.createTextNode()
+//untuk menambahkan paragraph baru
+//kita bikin elementnya dulu
+const pBaru=document.createElement('p')
+const tulisan= document.createTextNode('ini paragraf baru')
+
 //node.appendChild()
+pBaru.appendChild(tulisan) 
+//cara tampilkan ke document dengan menambahkan pBaru ke sectionA
+const sectionA=document.getElementById('a')
+sectionA.appendChild(pBaru)
+
 //node.insertBefore()
+//bikin element penampung li
+const newLi =document.createElement('li')
+const isiElement =document.createTextNode('Farhan')
+newLi.appendChild(isiElement)
+
+const sectionB =document.querySelector('#b ul')
+const penampung=sectionB.querySelector('li:nth-child(2)')
+sectionB.insertBefore(newLi,penampung)
+
+
+
+//coba coba
+//buat section c baru
+const sectionC=document.createElement('Section','C')
+sectionC.setAttribute('id','c')
+
+//section c udh dibuat dimasukin ke container
+const container=document.getElementById('container')
+container.appendChild(sectionC)
+
+//bikin isian dari section C
+const cobaIsi=document.createElement('p')
+const isian=document.createTextNode('section baru')
+cobaIsi.appendChild(isian)
+sectionC.appendChild(cobaIsi)
+
+
 //parentNode.removeChild()
+const elementYangAkanDihapus=document.getElementsByTagName('a')[0]
+sectionA.removeChild(elementYangAkanDihapus)
+
 //parentNode.replaceChild()
+const h2=document.createElement('h2')
+const isiH2=document.createTextNode('Diganti jadi H2')
+h2.appendChild(isiH2)
+
+const sectionB2=document.getElementById('b')
+const p4=sectionB2.querySelector('p')
+
+sectionB2.replaceChild(h2,p4)
+
+
+
+
+
+
+
+
+
+
